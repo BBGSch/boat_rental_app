099a57b931ca41ae2533ee96fef7f480b58cb55c
Bram Schevenels <bram.schevenels@gmail.com>
2020-08-26 14:02:40 +0200

    seed pictures and cloudinary

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem 'jbuilder', '~> 2.7'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'geocoder'
 
+gem 'cloudinary', '~> 1.12.0'
+
+
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 gem 'devise'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require "open-uri"
+
 roman = User.create(email: "user@example.com", password: "123ABC", name: "Roman Abramovich")
 puts roman
 jane = User.create(email: "user2@example.com", password: "123ABC", name: "Jane Abramovich")
@@ -38,44 +40,19 @@ puts big_boat
 
 
 
+boatypic = URI.open('https://i.kym-cdn.com/entries/icons/facebook/000/020/374/boaty---.jpg')
+boaty.photo.attach(io: boatypic, filename: 'boatypic.png', content_type: 'image/png')
 
+titanicpic = URI.open('https://media.nationalgeographic.org/assets/photos/000/273/27302_c0-41-990-701_r1050x700.jpg?d4ccf3044d9da0d0118103be3a76bd1319370847')
+titanic.photo.attach(io: titanicpic, filename: 'titanicpic.png', content_type: 'image/png')
 
-# Ingredient.create(name: "lemon")
-# Ingredient.create(name: "ice")
-# Ingredient.create(name: "mint leaves")
-# Ingredient.create(name: "Light rum")
-# Ingredient.create(name: "Applejack")
-# Ingredient.create(name: "Light rum")
-# Ingredient.create(name: "Gin")
-# Ingredient.create(name: "Dark rum")
-# Ingredient.create(name: "Sweet Vermouth")
-# Ingredient.create(name: "Strawberry schnapps")
-# Ingredient.create(name: "Champagne")
-# Ingredient.create(name: "Bitters")
-# Ingredient.create(name: "Sugar")
-# Dose.create(description: "4cl dark rum", cocktail_id: 1 , ingredient_id: 7)
-# Dose.create(description: "lemon", cocktail_id: 1 , ingredient_id: 1)
-# Dose.create(description: "ice", cocktail_id: 1 , ingredient_id: 2)
-# Dose.create(description: "ice", cocktail_id: 3 , ingredient_id: 1)
-# Dose.create(description: "gin", cocktail_id: 3 , ingredient_id: 7)
-# Dose.create(description: "lemon", cocktail_id: 3 , ingredient_id: 1)
-# darknstormy = Cocktail.create(name: "Dark & Stormy")
-# whiterussian = Cocktail.create(name: "White Russian")
-# tomcollins = Cocktail.create(name: "Tom Collins")
-# pornstarmartini = Cocktail.create(name: "Pornstar Martini")
-# oldfashioned = Cocktail.create(name: "Old Fashioned")
-# piscosour = Cocktail.create(name: "Pisco Sour")
-# darknstormypic = URI.open('https://www.liquor.com/thmb/vVQTKLEt9sxWKFFTCycXwkjOgrU=/735x0/dark-and-stormy-720x720-recipe-95f288ed6697444ab3301f8670b4564e.jpg')
-# darknstormy.photo.attach(io: darknstormypic, filename: 'darknstormypic.png', content_type: 'image/png')
-# whiterussianpic = URI.open('https://i.pinimg.com/originals/af/d2/4c/afd24c5047e6aaa1cfbe4dd8e8d7468f.jpg')
-# whiterussian.photo.attach(io: whiterussianpic, filename: 'whiterussianpic.png', content_type: 'image/png')
-# tomcollinspic = URI.open('https://thehomecocktail.club/wp-content/uploads/2020/03/tom-collins-cocktail-1.jpg')
-# tomcollins.photo.attach(io: tomcollinspic, filename: 'tomcollinspic.png', content_type: 'image/png')
-# pornstarmartinipic = URI.open('https://dronkenginger.nl/wp-content/uploads/2019/02/Porn-Star-martini-400x500.jpg')
-# pornstarmartini.photo.attach(io: pornstarmartinipic, filename: 'pornstarmartinipic.png', content_type: 'image/png')
-# oldfashionedpic = URI.open('https://d32miag6ta013h.cloudfront.net/master_cocktails/2869/en-us/small/old-fashioned-sml-470x594.jpg')
-# oldfashioned.photo.attach(io: oldfashionedpic, filename: 'oldfashionedpic.png', content_type: 'image/png')
-# piscosourpic = URI.open('https://alushlifemanual.com/wp-content/uploads/2020/04/Pisco-Sour.jpg')
-# piscosour.photo.attach(io: piscosourpic, filename: 'piscosourpic.png', content_type: 'image/png')
+hollandpic = URI.open('https://travelandynews.com/wp-content/uploads/2020/01/56523192_10156483754543406_5725163698733973504_o.jpg')
+holland.photo.attach(io: hollandpic, filename: 'hollandpic.png', content_type: 'image/png')
+
+onassispic = URI.open('https://travelandynews.com/wp-content/uploads/2020/01/56523192_10156483754543406_5725163698733973504_o.jpg')
+onassis.photo.attach(io: onassispic, filename: 'onassispic.png', content_type: 'image/png')
+
+big_boatpic = URI.open('https://lh3.googleusercontent.com/proxy/0do-AtyvCSLGxM723dIH3Rnyb4NrXbHLjsk30LMOHj-hWJC4B1mtKTy_-dAeBvuA3kejzKJARyQ2ZbzRqTdbzxo3BmMd_wlMy8yFwElo5A')
+big_boat.photo.attach(io: big_boatpic, filename: 'big_boatpic.png', content_type: 'image/png')
 
 
