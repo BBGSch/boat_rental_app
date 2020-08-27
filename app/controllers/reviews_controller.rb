b23a284236ed04736bf723867012a7f9c1187c15
Daan Roelofs <daanroelofs599@gmail.com>
2020-08-27 15:18:28 +0200

    reviews continued

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,12 +6,13 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    @reservation = Reservation.find(params[:reservation_id])
   end
 
   def create
     @review = Review.new(review_params)
     @review.user = current_user
-    # @review.reservation = params[:id]
+    @review.reservation = params[:id]
     raise
   end
 
