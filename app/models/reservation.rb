a8544761a75dcb18eed35d5456ce75ea782b7324
Bram Schevenels <bram.schevenels@gmail.com>
2020-08-27 16:37:44 +0200

    add completed views for reservations new and edit and halfway with view for show

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,13 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :boat
   has_many :reviews
+  before_validation :calculate_total_price
+
+def calculate_total_price
+  Self.total_price = (Self.end_date - Self.start_date)*Self.boat.price
 end
+
+
+
+end
+
